250f5ff1a9163f49f39bdc12bb6de5abfd767756
argianardi <argianardi1435@gmail.com>
2022-09-01 06:54:22 +0700

    clean code

--- src/pages/NowPlayingMovies.jsx
+++ src/pages/NowPlayingMovies.jsx
@@ -3,18 +3,11 @@ import CardMovies from "../components/CardMovies";
 import NavigationBar from "../components/NavigationBar";
 import axios from "axios";
 
-// import "./NowPlaying.css";
 const baseUrl = "https://api.themoviedb.org/";
 const page = 1;
-// const urlHeadline =
-//   baseUrl +
-//   "top-headlines?" +
-//   "country=id&" +
-//   `apiKey=${process.env.REACT_APP_API_KEY}`;
 
 let urlMovies = `${baseUrl}3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`;
 
-// let urlMovies = `https://api.themoviedb.org/3/movie/now_playing?api_key=f8bc7465b788992612b64c92537d503b&language=en-US&page=1`;
 export default class NowPlayingMovies extends Component {
   state = {
     ListMovies: [],
@@ -23,7 +16,6 @@ export default class NowPlayingMovies extends Component {
   componentDidMount() {
     const self = this;
     axios.get(urlMovies).then((response) => {
-      // console.log(response.data.results);
       self.setState({
         ListMovies: response.data.results,
       });
@@ -31,7 +23,6 @@ export default class NowPlayingMovies extends Component {
   }
 
   render() {
-    // const { ListMovies } = this.state;
     return (
       <>
         <NavigationBar />
