169f2b0ef9a182c80b41cf297f0c4ec12998832b
argianardi <argianardi1435@gmail.com>
2022-09-01 01:21:22 +0700

    add Card

--- src/components/CardMovies.jsx
+++ src/components/CardMovies.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 
 import "./CardMovies.css";
 
 export default class CardMovies extends Component {
   render() {
     return (
-      <div className="container-card">
-        <div className="picture">
+      <div>
+        {/* <div className="">
           <img src={this.props.src} alt={this.props.movie} />
         </div>
-        <h3 className="title"> {this.props.title} </h3>
+        <h3 className="text-white"> {this.props.title} </h3> */}
+        {/* <Row> */}
+        <Card className="bg-dark container-card my-3">
+          <img
+            src={this.props.src}
+            alt="this.props.title"
+            className="img-movie"
+          />
+          <h6 className="text-center text-white"> {this.props.title} </h6>
+        </Card>
       </div>
     );
   }
