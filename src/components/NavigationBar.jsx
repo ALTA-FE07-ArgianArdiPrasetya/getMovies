5c84668d9fa2afcf36ccba7289ffd2ac629052ca
argianardi <argianardi1435@gmail.com>
2022-09-01 19:24:11 +0700

    add link for navbar menu

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import { Navbar, Container, Nav, NavbarBrand } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class NavigationBar extends Component {
   render() {
     return (
       <>
         <Navbar sticky="top" className="bg-dark" variant="dark">
-          <Container>
-            <Nav>
-              <NavbarBrand className="text-light"> Bukan Bioskop</NavbarBrand>
-              <Nav.Link>Home</Nav.Link>
+          <Container className="d-flex align-content-center">
+            <Nav className="d-flex align-content-center ">
+              <NavbarBrand className="text-light">
+                <Link className="text-decoration-none" to="/">
+                  Bukan Bioskop
+                </Link>{" "}
+              </NavbarBrand>
+              <Nav.Link>
+                <Link className="text-decoration-none" to="/">
+                  Home{" "}
+                </Link>{" "}
+              </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link>Favorites</Nav.Link>
+              <Nav.Link>
+                {" "}
+                <Link className="text-decoration-none" to="/">
+                  Favorites{" "}
+                </Link>{" "}
+              </Nav.Link>
             </Nav>
           </Container>
         </Navbar>
