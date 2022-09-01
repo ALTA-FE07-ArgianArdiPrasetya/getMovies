0741f75653df44983ce2bc1229fe88dac4e4b47d
argianardi <argianardi1435@gmail.com>
2022-09-01 19:17:54 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
-// import NowPlayingMovies from "./pages/NowPlayingMovies";
+import NowPlayingMovies from "./pages/NowPlayingMovies";
 
-function App() {
-  return <>{/* <NowPlayingMovies /> */}</>;
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetailMovie from "./pages/DetailMovie";
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<NowPlayingMovies />} />
+          <Route path="/detail-movie" element={<DetailMovie />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
